fdbc47d9232e76a5593ac39ee3cb6f2954a4c825
sergio <srgmatveev@gmail.com>
2019-04-12 17:31:37 +0300

    hw09 add url build

--- work09/src/main/java/ru/hw09/jdbc/connection/MySQLConnectionHelper.java
+++ work09/src/main/java/ru/hw09/jdbc/connection/MySQLConnectionHelper.java
@@ -3,14 +3,14 @@ package ru.hw09.jdbc.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class MySQLConnectionHelper implements ConnectionHelper {
     private String propertyFile;
 
     public MySQLConnectionHelper() {
         super();
-        propertyFile="db";
+        propertyFile = "db";
     }
 
     public MySQLConnectionHelper(String propertyFile) {
@@ -39,12 +39,75 @@ public class MySQLConnectionHelper implements ConnectionHelper {
         }
     }
 
-    private String buildUrl(){
-        String url="";
-        return  url;
+    private String buildUrl() {
+        StringBuilder url = new StringBuilder();
+        url.append(addParam("url"));
+        url.append(addParam("host"));
+        url.append(":");
+        url.append(addParam("port", "3306"));
+        url.append("/");
+        url.append(addParam("database"));
+        url.append(addUrlParams(new String[]{"user", "password", "useSSL"}));
+        return url.toString();
     }
 
-    private ResourceBundle getBundle(){
+    private String addParam(String str) {
+        ResourceBundle rb = getBundle();
+        Optional<String> opt = getParamPropertiesFile(rb, str);
+        if (opt.isPresent())
+            return opt.get();
+        return "";
+    }
+
+    private String addParam(String str, String def) {
+        ResourceBundle rb = getBundle();
+        Optional<String> opt = getParamPropertiesFile(rb, str);
+        if (opt.isPresent())
+            return opt.get();
+        if (def != null) return def;
+        return "";
+    }
+
+    private String addUrlParams(String... str) {
+        ResourceBundle rb = getBundle();
+        Optional<String> opt;
+
+        List<String> lst = new ArrayList<>();
+        for (String string : str) {
+            opt = getParamPropertiesFile(rb, string);
+            if (opt.isPresent())
+                lst.add(String.format("%s=%s", string, opt.get()));
+        }
+
+        int n = lst.size();
+        if (n > 0) {
+            StringBuilder sb = new StringBuilder("?");
+
+            for (int i = 0; i < n; ++i) {
+                sb.append(lst.get(i));
+                if (i < (n - 1)) sb.append("&");
+            }
+            return sb.toString();
+        }
+
+        return "";
+    }
+
+    private Optional<String> getParamPropertiesFile(ResourceBundle resourceBundle, String str) {
+
+        if (resourceBundle == null) return Optional.empty();
+        if (str == null || str.isEmpty()) return Optional.empty();
+        String res = null;
+        try {
+            res = resourceBundle.getString(str);
+        } catch (MissingResourceException e) {
+            return Optional.empty();
+        }
+        return res.isEmpty() ? Optional.empty() : Optional.of(res);
+
+    }
+
+    private ResourceBundle getBundle() {
         ResourceBundle rb = ResourceBundle.getBundle(propertyFile);
         return rb;
     }
--- work09/src/test/java/ru/hw09/jdbc/connection/MySQLConnectionHelperTest.java
+++ work09/src/test/java/ru/hw09/jdbc/connection/MySQLConnectionHelperTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MySQLConnectionHelperTest {
     private ConnectionHelper connectionHelper;
+
     @BeforeEach
     void setUp() {
         connectionHelper = new MySQLConnectionHelper("db_test");
@@ -21,10 +24,10 @@ class MySQLConnectionHelperTest {
     void buildUrl() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method method = connectionHelper.getClass().getDeclaredMethod("buildUrl");
         method.setAccessible(true);
-        assertEquals(method.getName(),"buildUrl");
+        assertEquals(method.getName(), "buildUrl");
 
         String url = (String) method.invoke(connectionHelper);
-        assertEquals(url,"");
+        assertEquals(url, "jdbc:mysql://127.1.1:3310/hw09?user=dbuser&password=dbuserpassword&useSSL=false");
 
     }
 
@@ -33,11 +36,29 @@ class MySQLConnectionHelperTest {
     void resourcesBundle() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method method = connectionHelper.getClass().getDeclaredMethod("getBundle");
         method.setAccessible(true);
-        assertEquals(method.getName(),"getBundle");
+        assertEquals(method.getName(), "getBundle");
         ResourceBundle rb = (ResourceBundle) method.invoke(connectionHelper);
         assertNotNull(rb);
-        System.out.println(rb.getString("url"));
+        assertEquals(rb.getString("url"), "jdbc:mysql://");
 
     }
 
+    @Test
+    void getParamPropertiesFile() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = connectionHelper.getClass().getDeclaredMethod("getBundle");
+        method.setAccessible(true);
+        ResourceBundle rb = (ResourceBundle) method.invoke(connectionHelper);
+
+        Class[] cArg = new Class[]{java.util.ResourceBundle.class, java.lang.String.class};
+        method = connectionHelper.getClass().getDeclaredMethod("getParamPropertiesFile",cArg);
+        method.setAccessible(true);
+        assertEquals(method.getName(), "getParamPropertiesFile");
+
+        Optional<String> name = (Optional<String>) method.invoke(connectionHelper, rb, "url");
+        assertEquals(name.get(),"jdbc:mysql://");
+        name = (Optional<String>) method.invoke(connectionHelper, rb, "test_url");
+        assertFalse(name.isPresent());
+        name = (Optional<String>) method.invoke(connectionHelper, rb, "eees");
+        assertFalse(name.isPresent());
+    }
 }
