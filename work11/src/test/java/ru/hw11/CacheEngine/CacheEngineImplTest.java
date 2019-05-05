f6a8ccc3d3911a3f21c406038f3c85148d87afe3
sergio <srgmatveev@gmail.com>
2019-05-05 21:00:41 +0300

    hw 11 tests

---
+++ work11/src/test/java/ru/hw11/CacheEngine/CacheEngineImplTest.java
@@ -0,0 +1,59 @@
+package ru.hw11.CacheEngine;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.powermock.reflect.Whitebox;
+import ru.hw11.base.cache.CacheEngine;
+import ru.hw11.base.cache.MyElement;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+
+public class CacheEngineImplTest {
+
+    private CacheEngine<Long,String> cacheEngine;
+
+    @Before
+    public void setUp() throws Exception {
+        cacheEngine = new CacheEngineImpl(5,2000,0,false);
+    }
+
+
+    public void put() {
+     cacheEngine.put(new MyElement<Long, String>(1L,"Sergio"));
+
+    }
+
+    @Test
+    public void get() {
+     put();
+     assertEquals(cacheEngine.get(1L).getValue(),"Sergio");
+    }
+
+    @Test
+    public void getHitCount() throws NoSuchFieldException, IllegalAccessException {
+        Field hit = cacheEngine.getClass().getDeclaredField("hit");
+        hit.setAccessible(true);
+        hit.setInt(cacheEngine,119);
+        assertEquals(cacheEngine.getHitCount(),119);
+    }
+
+    @Test
+    public void getMissCount() throws NoSuchFieldException, IllegalAccessException {
+        Field miss = cacheEngine.getClass().getDeclaredField("miss");
+        miss.setAccessible(true);
+        miss.setInt(cacheEngine,2251);
+        assertEquals(cacheEngine.getMissCount(),2251);
+    }
+
+    @Test
+    public void dispose() {
+        cacheEngine.dispose();
+    }
+}
